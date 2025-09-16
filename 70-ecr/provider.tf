74434eb79751d4d373727a1aa2c20a14df3efedd
durgaprasadkothapalli <durgaprasadkothapalli908@gmail.com>
2025-09-16 17:02:34 +0530

    probes

--- 70-ecr/provider.tf
+++ 70-ecr/provider.tf
@@ -5,9 +5,17 @@ terraform {
       version = "6.13.0"
     }
   }
-}
+  
+
+  backend "s3" {
+    bucket = "81s-remote-state"
+    key    = "expense-dev-acm"
+    region = "us-east-1"
+    dynamodb_table = "81s-locking"
+  }
 
 provider "aws" {
   # Configuration options
   region = "us-east-1"
+}
 }
